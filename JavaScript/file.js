c1ac6bdeed9f16cf446876a5ee8d5ad9bdbeb574
GasparAmato <81720763+GasparAmato@users.noreply.github.com>
2021-06-29 23:07:36 -0300

    Add files via upload

---
+++ JavaScript/file.js
@@ -0,0 +1,76 @@
+
+
+
+//  $.getJSON("JSON/producto.json", (response) => { console.log(response)});
+
+
+
+//  console.log(arrayDeCards)
+
+
+ 
+
+let carrito = new Array();
+let carroConsole = [];
+let carritoJSON = []
+
+const cardIds = ["cama1", "cama2", "cama3", "cama4", "cushion1", "cushion2", "cushion3", "cushion4"];
+
+
+let cartIf = localStorage.getItem("CarritoArray")
+
+if (cartIf){
+   carrito  = JSON.parse(cartIf);
+   $("#cart").html(carrito.length);
+  }else{
+    carrito = []
+  } 
+
+  
+
+console.log(carrito)
+ 
+function asignaEventos(identificador, arrayDeCards) {
+
+  document.getElementById(identificador).addEventListener("click", function() {
+
+    console.log(arrayDeCards)
+
+    var arrayAlCarrito = arrayDeCards.find(({name}) => name === identificador)
+    carrito.push(arrayAlCarrito)
+    carritoJSON = JSON.stringify(carrito)
+    
+    localStorage.setItem("CarritoArray", carritoJSON )
+
+    // contadorCart = contadorCart + 1;
+    // localStorage.setItem("contadorCart", contadorCart);
+    // $("#cart").html( localStorage.getItem("contadorCart"));
+    $("#cart").html(carrito.length)
+
+    carroConsole.push(identificador)
+    console.log(carroConsole);  
+    console.log(arrayAlCarrito) 
+    console.log(carrito) 
+
+  });
+}
+
+
+$.getJSON("JSON/productos.json", function (respuesta, estado) {
+  if(estado === "success"){
+  const arrayDeCards = respuesta;
+    for (const identificador of cardIds) {
+      asignaEventos(identificador, arrayDeCards);
+    };
+  }
+})
+
+
+
+
+
+
+
+
+
+
